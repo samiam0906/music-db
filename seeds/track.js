8795e46a4d9d26afa1202c340309b430d3832731
Sam Hoffman <samdhoffman@gmail.com>
2017-10-13 10:22:23 -0400

    finish exercise

--- seeds/track.js
+++ seeds/track.js
@@ -5,8 +5,8 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('track').insert([
-        {id: 1, name: 'Strawberry Fields'},
-        {id: 2, name: 'Soft Bulletin'}
+        {id: 1, name: 'Strawberry Fields', artist_id: 1},
+        {id: 2, name: 'Soft Bulletin', artist_id: 2}
       ]);
     });
 };
